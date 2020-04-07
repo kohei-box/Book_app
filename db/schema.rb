ce8b5398ec614c5ae5a275503ec50bb332c4e341
EC2 Default User <ec2-user@ip-172-31-28-253.us-east-2.compute.internal>
2020-04-07 07:39:49 +0000

    bookテーブルにuser(FK)追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_065449) do
+ActiveRecord::Schema.define(version: 2020_04_07_073732) do
 
   create_table "books", force: :cascade do |t|
     t.string "googlebooksapi_id", null: false
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 2020_04_07_065449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "page_count"
+    t.integer "user_id"
     t.index ["author", "title"], name: "index_books_on_author_and_title"
     t.index ["googlebooksapi_id"], name: "index_books_on_googlebooksapi_id", unique: true
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
